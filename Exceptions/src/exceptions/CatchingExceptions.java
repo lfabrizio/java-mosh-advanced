40322f45c568a279d561c173f9d161f27fa653ed
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-15 17:30:05 -0400

    tryblock

---
+++ Exceptions/src/exceptions/CatchingExceptions.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CatchingExceptions {
+    public static void show() {
+        try {
+            var reader = new FileReader("file.txt");
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("File Opened");
+    }
+}
