1a53b49a55666d5b91578635b0e7a52b3b9c3e9f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-16 17:58:06 -0400

    catching multiple exceptions

--- Exceptions/src/exceptions/CatchingMultiple.java
+++ Exceptions/src/exceptions/CatchingMultiple.java
@@ -3,18 +3,19 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 // catching multiples
 public class CatchingMultiple {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
-           var value = reader.read();
+            var value = reader.read();
+            new SimpleDateFormat().parse("");
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e){
+        catch (IOException | ParseException e) {
+
             System.out.println("could not read data");
         }
     }
