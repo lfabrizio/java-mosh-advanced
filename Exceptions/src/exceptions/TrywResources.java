119f46f0cda9b6a38deb7dc6b4c721f3559cee6c
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-18 14:11:20 -0400

    trywithresources

--- Exceptions/src/exceptions/TrywResources.java
+++ Exceptions/src/exceptions/TrywResources.java
@@ -1,5 +1,20 @@
 package exceptions;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TrywResources {
-    
+    public static void show() {
+
+        try (
+                var reader = new FileReader("file.txt")){
+                var writer = reader.read();
+            {
+                var value = reader.read();
+            }
+        } catch (
+                IOException e) {
+            System.out.println("could not read data");
+        }
+    }
 }
