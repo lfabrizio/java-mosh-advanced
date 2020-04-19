ed90cd8fa2019101367fa686e12b7d9675b294bf
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-19 14:35:13 -0400

    need for generics

---
+++ Generics/src/com/fabrizio/Main.java
@@ -0,0 +1,9 @@
+package com.fabrizio;
+
+public class Main {
+
+    public static void main(String[] args) {
+	var list = new List();
+	list.add(1);
+    }
+}
---
+++ Generics/src/com/fabrizio/User.java
@@ -0,0 +1,4 @@
+package com.fabrizio;
+
+public class User {
+}
