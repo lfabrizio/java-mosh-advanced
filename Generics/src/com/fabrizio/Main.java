ed08894a64ca31fdbf1cd8c7cc3a13fb0e7ba116
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-19 15:34:47 -0400

    primitive types

--- Generics/src/com/fabrizio/Main.java
+++ Generics/src/com/fabrizio/Main.java
@@ -3,8 +3,10 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-	var list = new GenericList<User>();
-	list.add(new User);
-	User user = list.get(0);
+
+	 GenericList<Integer> numbers = new  GenericList<>();
+	 numbers.add(1); //boxing
+        int number = numbers.get(0); //unboxing
+
     }
 }
