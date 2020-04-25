bbbcde5f9c27bd978feb54d81eef5c6e920aa243
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-25 15:26:05 -0400

    hash table

---
+++ Collections/src/com/fabrizio/collections/MapDemo.java
@@ -0,0 +1,19 @@
+package com.fabrizio.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapDemo {
+    public static void show(){
+//        List<Customer> customers = new ArrayList<>();
+//        // o(n) big o of n. 10 items would be o of 10= o(10)
+//        for (var customer : customers)
+//            if (customer.getEmail() == "e1")
+//                System.out.println("found!");
+        //hash tables
+        // Java: Maps
+        // C# Dictionary
+        // Javascript: Objects
+        // var person = { name: "a" }
+    }
+}
