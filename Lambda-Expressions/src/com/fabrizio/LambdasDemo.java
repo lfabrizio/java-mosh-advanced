d5369484f0f70165d5d07d61d4902924fc1b5fad
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-27 19:38:27 -0400

    chaining consumers

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -1,16 +1,15 @@
 package com.fabrizio;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        List<Integer> list = List.of(1, 2, 3);
-        // Imperative Programming(for, if/else, switch/case)
-        for (var item : list)
-            System.out.println(item);
+        List<String> list = List.of("a", "b", "c");
+        Consumer<String> print = item -> System.out.println(item);
+        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
 
-            // Declarative Programming
-        list.forEach(item -> System.out.println(item));
+        list.forEach(print.andThen(printUpperCase));
     }
 }
