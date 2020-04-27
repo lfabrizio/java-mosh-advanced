a8077824c1bc29104960ac88cd5172fe5c0d8c40
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-27 17:26:30 -0400

    variable capture

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -1,13 +1,13 @@
 package com.fabrizio;
-
+//Lamdas standalone function without belonging to a class
 public class LambdasDemo {
-    public static void show(){
-        greet(message -> System.out.println(message));
+    public static String prefix = "-";
 
-        Printer printer = message -> System.out.println(message);
+    public static void show(){
+        greet(message -> System.out.println( prefix+ message));
         };
 
     public static void greet(Printer printer) {
-        printer.print("Hello");
+        printer.print("Hello World");
     }
 }
