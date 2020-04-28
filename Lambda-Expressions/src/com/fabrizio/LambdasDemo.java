8df308cf82b102474e41c7b65ad573d104b37e5b
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-28 18:09:18 -0400

    predicate interface

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -3,22 +3,13 @@ package com.fabrizio;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        // "key: value"
-        // first: "key = value"
-        // second: "{key=value}"
-        Function<String, String> replaceColon = str -> str.replace(":", "=");
-        Function<String, String> addBraces = str -> "{" + str + "}";
-    // declarative programming
-        var result = replaceColon
-                .andThen(addBraces)
-                .apply("key:value");
-
-        result = addBraces.compose(replaceColon).apply("key:value");
-        System.out.println(result);
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+        var result = isLongerThan5.test("sky");
     }
 }
