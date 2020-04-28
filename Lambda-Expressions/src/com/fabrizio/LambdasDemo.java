24debf17de55e63c41ea4726dcd46b94ec4b86fc
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-27 20:06:57 -0400

    the supplier interface

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -2,14 +2,13 @@ package com.fabrizio;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        List<String> list = List.of("a", "b", "c");
-        Consumer<String> print = item -> System.out.println(item);
-        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
-
-        list.forEach(print.andThen(printUpperCase));
+        Supplier<Double> getRandom = () -> Math.random();
+        var random = getRandom.get();
+        System.out.println(random);
     }
 }
