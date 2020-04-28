5eee93fb6d155b96539c1168a234424c8c937c7d
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-28 19:53:37 -0400

    streams

---
+++ Streams/src/com/fabrizio/StreamsDemo.java
@@ -0,0 +1,14 @@
+package com.fabrizio;
+
+import java.util.List;
+
+public class StreamsDemo {
+    public static void show(){}
+    List<Movie> movies = List.of(
+            new Movie("a", 10),
+            new Movie("b", 15),
+            new Movie("c", 20)
+    );
+    // imperative programming
+    
+}
