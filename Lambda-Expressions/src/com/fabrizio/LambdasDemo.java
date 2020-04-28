33e95b73cf3cba7c0010f011a6c226e400f6a623
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-28 18:38:55 -0400

    combining predicates

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -9,7 +9,9 @@ import java.util.function.Supplier;
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        Predicate<String> isLongerThan5 = str -> str.length() > 5;
-        var result = isLongerThan5.test("sky");
+        Predicate<String> hasLeftBrace = str -> str.startsWith("{");
+        Predicate<String> hasRightBrace = str -> str.startsWith("}");
+
+        hasLeftBrace.negate();
     }
 }
