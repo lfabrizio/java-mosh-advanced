4552f3dea84cc17170a9297977db371424ba5443
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-28 17:10:39 -0400

    function interface

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -2,13 +2,14 @@ package com.fabrizio;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        Supplier<Double> getRandom = () -> Math.random();
-        var random = getRandom.get();
-        System.out.println(random);
+        Function<String, Integer> map = str -> str.length();
+       var length = map.apply("Sky");
+        System.out.println(length);
     }
 }
