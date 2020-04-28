4a0b261a5c30fdeced9ad11e8c0255c310744be7
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-28 18:02:55 -0400

    composing function

--- Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
+++ Lambda-Expressions/src/com/fabrizio/LambdasDemo.java
@@ -8,8 +8,17 @@ import java.util.function.Supplier;
 //Lamdas standalone function without belonging to a class
 public class LambdasDemo {
     public static void show() {
-        Function<String, Integer> map = str -> str.length();
-       var length = map.apply("Sky");
-        System.out.println(length);
+        // "key: value"
+        // first: "key = value"
+        // second: "{key=value}"
+        Function<String, String> replaceColon = str -> str.replace(":", "=");
+        Function<String, String> addBraces = str -> "{" + str + "}";
+    // declarative programming
+        var result = replaceColon
+                .andThen(addBraces)
+                .apply("key:value");
+
+        result = addBraces.compose(replaceColon).apply("key:value");
+        System.out.println(result);
     }
 }
