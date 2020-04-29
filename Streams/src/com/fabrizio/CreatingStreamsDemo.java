dbe9b7abb09a50dfcf48b19f63d06200f99051ce
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-29 18:30:05 -0400

    mapping elements

--- Streams/src/com/fabrizio/CreatingStreamsDemo.java
+++ Streams/src/com/fabrizio/CreatingStreamsDemo.java
@@ -11,6 +11,6 @@ public class CreatingStreamsDemo {
     public static void show() {
         Stream.iterate(1, n -> n + 1)
                 .limit(10)
-                .forEach(n -> System.out.println(n););
+                .forEach(n -> System.out.println(n));
     }
 }
--- Streams/src/com/fabrizio/StreamsDemo.java
+++ Streams/src/com/fabrizio/StreamsDemo.java
@@ -1,25 +1,16 @@
 package com.fabrizio;
 
-//import java.util.List;
-//
-//public class StreamsDemo {
-//    public static void show() {
-//    }
-//
-//    List<Movie> movies = List.of(
-//            new Movie("a", 10),
-//            new Movie("b", 15),
-//            new Movie("c", 20)
-//    );
-//    // imperative programming
-////    int count = 0;
-////    for(
-////    var movie :movies)
-////            if(movie.getLikes()>10)
-////    count++;
-//// declarative or functional programming
-//            var count2 = movies.stream()
-//                .filter(movie -> movie.getLikes(0 > 10)
-//                .count();
-//}
-//}
+import java.util.List;
+
+public class StreamsDemo {
+    public static void show(){
+        var movies = List.of(
+                new Movie("a", 10),
+                new Movie("b", 20),
+                new Movie("c", 25)
+        );
+
+        movies.stream()
+                .map(movie -> movie.)
+    }
+}
