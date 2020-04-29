ec01918ec7e67b58709ec5bc269299686d751328
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-29 18:15:03 -0400

    functional declarative

--- Streams/src/com/fabrizio/StreamsDemo.java
+++ Streams/src/com/fabrizio/StreamsDemo.java
@@ -1,14 +1,25 @@
 package com.fabrizio;
 
-import java.util.List;
-
-public class StreamsDemo {
-    public static void show(){}
-    List<Movie> movies = List.of(
-            new Movie("a", 10),
-            new Movie("b", 15),
-            new Movie("c", 20)
-    );
-    // imperative programming
-    
-}
+//import java.util.List;
+//
+//public class StreamsDemo {
+//    public static void show() {
+//    }
+//
+//    List<Movie> movies = List.of(
+//            new Movie("a", 10),
+//            new Movie("b", 15),
+//            new Movie("c", 20)
+//    );
+//    // imperative programming
+////    int count = 0;
+////    for(
+////    var movie :movies)
+////            if(movie.getLikes()>10)
+////    count++;
+//// declarative or functional programming
+//            var count2 = movies.stream()
+//                .filter(movie -> movie.getLikes(0 > 10)
+//                .count();
+//}
+//}
