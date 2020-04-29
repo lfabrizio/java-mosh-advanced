9531f0545bb0e09e7348e54ae318f96cebdad53a
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-04-29 18:26:22 -0400

    creating a stream

---
+++ Streams/src/com/fabrizio/CreatingStreamsDemo.java
@@ -0,0 +1,16 @@
+package com.fabrizio;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class CreatingStreamsDemo {
+    public static void show() {
+        Stream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> System.out.println(n););
+    }
+}
