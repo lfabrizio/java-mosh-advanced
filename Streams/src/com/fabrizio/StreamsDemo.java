aaeb01556f9139041b53a60520c1e59dee922585
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-02 17:22:55 -0400

    reducing a stream

--- Streams/src/com/fabrizio/StreamsDemo.java
+++ Streams/src/com/fabrizio/StreamsDemo.java
@@ -4,6 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -16,12 +17,11 @@ public class StreamsDemo {
                 new Movie("b", 30)
         );
         // reducers
+        Integer sum = movies.stream()
+                .map(m -> m.getLikes())
+                .reduce(0, Integer::sum);
 
-       var result = movies.stream()
-                .max(Comparator.comparing(Movie::getLikes))
-               .get();
-
-        System.out.println(result.getTitle());
+        System.out.println(sum);
 
     }
 }
