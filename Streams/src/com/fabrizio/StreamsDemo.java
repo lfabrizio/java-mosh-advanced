b612e54b542b56cd1980a97d052b1bc62db32796
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-04 19:17:38 -0400

    primitive streams

--- Streams/src/com/fabrizio/StreamsDemo.java
+++ Streams/src/com/fabrizio/StreamsDemo.java
@@ -3,20 +3,14 @@ package com.fabrizio;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class StreamsDemo {
     public static void show() {
-        var movies = List.of(
-                new Movie("a", 10, Genre.ACTION),
-                new Movie("a", 20, Genre.THRILLER),
-                new Movie("b", 30, Genre.COMEDY)
-        );
-         var result = movies.stream()
-                .collect(Collectors.partitioningBy(
-                        m -> m.getLikes() > 20,
-                        Collectors.mapping(Movie::getTitle,
-                        Collectors.joining(",")));
-        System.out.println(result);
+        IntStream.rangeClosed(1, 5)
+                .forEach(System.out::println);
+
+
     }
 }
