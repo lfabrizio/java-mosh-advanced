065d44859d0bc7fe863f1cc9cd103b8cb3011c74
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-05 17:56:00 -0400

    processes and threads

--- Concurrency-MultiThreading/src/com/fabrizio/Main.java
+++ Concurrency-MultiThreading/src/com/fabrizio/Main.java
@@ -3,6 +3,7 @@ package com.fabrizio;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
