0eba4c8bd241bd37f1160ff26734a9bd72c68b49
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-06 18:26:03 -0400

    concurrency

--- Concurrency-MultiThreading/src/com/fabrizio/DownloadFileTAsk.java
+++ Concurrency-MultiThreading/src/com/fabrizio/DownloadFileTAsk.java
@@ -3,6 +3,6 @@ package com.fabrizio;
 public class DownloadFileTAsk  implements Runnable{
     @Override
     public void run() {
-        System.out.print("download a file" + Thread.currentThread().getName());
+        System.out.print("download a file" + Thread.currentThread().getId());
     }
 }
