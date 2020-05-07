94b26e001690b741b7b62a3a5b4c4ef743a43f18
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-05-07 17:44:27 -0400

    pausing a thread

--- Concurrency-MultiThreading/src/com/fabrizio/DownloadFileTAsk.java
+++ Concurrency-MultiThreading/src/com/fabrizio/DownloadFileTAsk.java
@@ -4,5 +4,13 @@ public class DownloadFileTAsk  implements Runnable{
     @Override
     public void run() {
         System.out.print("download a file" + Thread.currentThread().getId());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete" + Thread.currentThread().getName());
     }
 }
